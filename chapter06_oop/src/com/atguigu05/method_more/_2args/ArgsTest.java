907f5487f4441baae9bb7cd0a14a3015dc2f2b26
Synergism <shenhewulv@gmail.com>
2023-05-31 14:33:07 +0800

    oop:可变形参

---
+++ chapter06_oop/src/com/atguigu05/method_more/_2args/ArgsTest.java
@@ -0,0 +1,14 @@
+package com.atguigu05.method_more._2args;
+
+public class ArgsTest {
+
+
+    public void print(int... nums) {
+    }
+
+    //与数组参数实质上一样，传入参数时不用创数组罢了
+//    public void print(int[] nums){}
+
+    //可变形参必须在形参列表末尾
+//    public void print(int... nums, String a) {}
+}
